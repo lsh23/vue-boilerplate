40dbb25a340f979e2cef6decdf96ed25c7ba3970
lsh23 <shseoul14@gmail.com>
2023-06-30 15:05:33 +0900

    chore : ESLint error overay 설정 끄기

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    client: {
+      overlay: false, // 화면에 ESLint error message 출력 끄기
+    },
+  },
 });
